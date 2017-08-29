cfc0a476c9421132be71425bf4a18639d2904e49
codehedgehog <codehedgehog@outlook.com>
2017-08-29 13:59:32 -0700

    Consuming a class library with .NET Core in Visual Studio 2017 https://docs.microsoft.com/en-us/dotnet/core/tutorials/consuming-library-with-visual-studio?tabs=csharp

--- NetCoreConsoleHelloWorld/Program.cs
+++ NetCoreConsoleHelloWorld/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
-
+﻿
 namespace NetCoreConsoleHelloWorld
 {
+	using System;
+	using UtilityLibraries;
+
 	class Program
 	{
 		static void Main(string[] args)
@@ -12,6 +14,23 @@ namespace NetCoreConsoleHelloWorld
 			Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
 			Console.Write("\nPress any key to exit...");
 			Console.ReadKey(true);
+
+			int rows = Console.WindowHeight;
+
+			Console.Clear();
+			do
+			{
+				if (Console.CursorTop >= rows || Console.CursorTop == 0)
+				{
+					Console.Clear();
+					Console.WriteLine("\nPress <Enter> only to exit; otherwise, enter a string and press <Enter>:\n");
+				}
+				string input = Console.ReadLine();
+				if (String.IsNullOrEmpty(input)) break;
+				Console.WriteLine($"Input: {input} {"Begins with uppercase? ",30}: " +
+													$"{(input.StartsWithUpper() ? "Yes" : "No")}\n");
+			} while (true);
+
 		}
 	}
 }
---
+++ StringLibrary/readme.md
@@ -0,0 +1,7 @@
+﻿### Reference Notes
+
+Distributing the library in a NuGet package
+```
+dotnet pack --no-build
+```
+The ```dotnet``` utility generates a package with a _.nupkg_ extension.
